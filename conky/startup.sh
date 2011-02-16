78a673148211ce362fbcea1b000abf1eca75ce8c
Patrick Scott <phanna@google.com>
2011-02-16 09:16:53 -0500

    Conky config files.

---
+++ conky/startup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+# Copyright 2010 Google Inc. All Rights Reserved.
+# Author: phanna@google.com (Patrick Scott)
+conky -c ~/.conky/panel1 &
+conky -c ~/.conky/panel2 &
+conky -c ~/.conky/panel3 &
